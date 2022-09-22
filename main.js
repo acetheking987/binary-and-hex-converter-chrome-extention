4119ae7005191af098c549cc618a205566e5e89a
acetheking987 <acetheking987@gmail.com>
2022-09-22 16:46:26 +0100

    update

--- main.js
+++ main.js
@@ -1,61 +1,62 @@
-document.getElementById("htb").addEventListener("click", htb);
-document.getElementById("bth").addEventListener("click", bth);
-document.getElementById("htn").addEventListener("click", htn);
-document.getElementById("nth").addEventListener("click", nth);
-document.getElementById("ntb").addEventListener("click", ntb);
-document.getElementById("btn").addEventListener("click", btn);
+document.getElementById("convert").addEventListener("click", convert);
+document.getElementById("mode").addEventListener("click", mode);
+document.getElementById("rightshift").addEventListener("click", rightshift);
+document.getElementById("leftshift").addEventListener("click", leftshift);
 
-function htb() {
+function rightshift() {
     //get input
-    var input = document.getElementById("input").value;
-    //convert hex to binary
-    var binary = parseInt(input, 16).toString(2);
+    var input = document.getElementById("shiftinput").value;
+    var length = input.length;
+    //ammount to shift
+    var shift = document.getElementById("shiftamount").value;
+    var amount = ""
+    for (var i = 0; i < shift; i++) {
+        amount += "0";
+    }
+    //shift binary
+    var shifted = (amount + input).substring(0, length);
     //output
-    document.getElementById("output").value = binary;
+    document.getElementById("shiftoutput").value = shifted;
 }
 
-function bth() {
+function leftshift() {
     //get input
-    var input = document.getElementById("input").value;
-    //convert binary to hex
-    var hex = parseInt(input, 2).toString(16);
+    var input = document.getElementById("shiftinput").value;
+    //ammount to shift
+    var shift = document.getElementById("shiftamount").value;
+    //shift binary
+    var shifted = input.substring(shift, input.length) + "0"*shift;
     //output
-    document.getElementById("output").value = hex;
+    document.getElementById("shiftoutput").value = shifted;
 }
 
+modes = ["number converter", "binary shift"];
+var mode_index = 0;
+function mode() {
+    mode_index += 1;
+    if (mode_index >= modes.length) {
+        mode_index = 0;
+    }
 
-function htn() {
-    //get input
-    var input = document.getElementById("input").value;
-    //convert hex to number
-    var number = parseInt(input, 16);
-    //output
-    document.getElementById("output").value = number;
+    console.log(mode_index);
+
+    for (var i = 0; i < modes.length; i++) {
+        document.getElementById(modes[i]).classList.remove("shown");
+        document.getElementById(modes[i]).classList.add("hidden");
+    }
+    document.getElementById(modes[mode_index]).classList.remove("hidden");
+    document.getElementById(modes[mode_index]).classList.add("shown");
 }
 
-function nth() {
+function convert() {
     //get input
     var input = document.getElementById("input").value;
-    //convert number to hex
-    var hex = parseInt(input, 10).toString(16);
+    //get from and to
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    console.log(from, to);
+    //convert
+    var converted = parseInt(input, from).toString(to);
     //output
-    document.getElementById("output").value = hex;
-}
-
-function ntb() {
-    //get input
-    var input = document.getElementById("input").value;
-    //convert number to binary
-    var binary = parseInt(input, 10).toString(2);
-    //output
-    document.getElementById("output").value = binary;
-}
-
-function btn() {
-    //get input
-    var input = document.getElementById("input").value;
-    //convert binary to number
-    var number = parseInt(input, 2);
-    //output
-    document.getElementById("output").value = number;
+    document.getElementById("output").value = converted;
 }
